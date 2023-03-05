ba6be383d81f5660692294bfada5d30db74d7f64
eva0supiot <supiot.eva@gmail.com>
2023-03-05 18:07:23 +0100

    Menu à mettre dans toutes les pages <!-- pour inclure le menu --> <?php include('menu.php'); ?>

---
+++ menu.php
@@ -0,0 +1,24 @@
+
+<head>
+	<link href="tout_parcourir.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<div id="titre"><img src="logo_bee.gif" alt="Java logo"height="100" width="550"></div>
+
+<nav>
+  <ul>
+    <li><a href="#">Accueil</a></li>
+
+    <li class="tout_parcourir"><a href="#">Tout Parcourir &ensp;</a>
+      <ul class="immo">
+        <li><a href="biens_maison.php">Immobilier résidentiel</a></li>
+        <li><a href="biens_commerce.php">Immobilier commercial</a></li>
+        <li><a href="biens_terrain.php">Terrain</a></li>
+        <li><a href="biens_appart.php">Appartement à louer</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Recherche</a></li>
+    <li><a href="#">Rendez-vous</a></li>
+    <li><a href="#">Mon Compte</a></li>
+  </ul>
+</nav>
