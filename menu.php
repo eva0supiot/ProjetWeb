a7b00789d669488dbfbff135ade302b580a26acf
PicotDesBois <79858497+PicotDesBois@users.noreply.github.com>
2023-03-07 10:49:04 +0100

    Update menu.php
    push fichier non à jour

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 
 <nav>
   <ul>
-    <li><a href="#">Accueil</a></li>
+    <li><a href="accueil.html">Accueil</a></li>
 
     <li class="tout_parcourir"><a href="#">Tout Parcourir &ensp;</a>
       <ul class="immo">
@@ -18,6 +18,6 @@
     </li>
     <li><a href="#">Recherche</a></li>
     <li><a href="#">Rendez-vous</a></li>
-    <li><a href="#">Mon Compte</a></li>
+    <li><a href="monCompte.php">Mon Compte</a></li>
   </ul>
 </nav>
